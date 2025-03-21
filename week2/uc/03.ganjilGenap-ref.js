db56355632c311bf4c1f002186c1d638e6c6cdee
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-03-21 23:14:18 +0700

    uc-ganjilGenap - add manual split to array

--- week2/uc/03.ganjilGenap-ref.js
+++ week2/uc/03.ganjilGenap-ref.js
@@ -1,5 +1,47 @@
 // *1 manual split to array
 
+function ganjilGenap(plat) {
+    let platGanjil = 0;
+    let platGenap = 0;
+    // console.log(ganjilGenap("2341;3429;864;1309;1276")); //plat genap sebanyak 2 dan plat ganjil sebanyak 3
+
+    if (!plat) {
+        return plat == "" ? "plat tidak ditemukan" : "invalid data";
+    } else {
+        let arrPlat = [];
+        let splitPlat = "";
+        for (let i = 0; i < plat.length; i++) {
+            const index = plat[i];
+            if (index != ";") {
+                splitPlat += index;
+            }
+            if (index === ";" || i === plat.length - 1) {
+                arrPlat.push(splitPlat);
+                splitPlat = "";
+            }
+        }
+
+        console.log(arrPlat);
+        for (let j = 0; j < arrPlat.length; j++) {
+            const index = arrPlat[j];
+            index % 2 == 0 ? platGenap++ : platGanjil++;
+        }
+
+        if (platGenap && platGanjil) {
+            return `plat genap sebanyak ${platGenap} dan plat ganjil sebanyak ${platGanjil}`;
+        } else if (!platGenap && platGanjil) {
+            return `plat ganjil sebanyak ${platGanjil} dan plat genap tidak ditemukan`;
+        } else if (platGenap && !platGanjil) {
+            return `plat genap sebanyak ${platGenap} dan plat ganjil tidak ditemukan`;
+        }
+    }
+}
+
+console.log(ganjilGenap("2341;3429;864;1309;1276")); //plat genap sebanyak 2 dan plat ganjil sebanyak 3
+console.log(ganjilGenap("2347;3429;1305")); //plat ganjil sebanyak 3 dan plat genap tidak ditemukan
+console.log(ganjilGenap("864;1308;1276;1432")); //plat genap sebanyak 4 dan plat ganjil tidak ditemukan
+console.log(ganjilGenap("")); //plat tidak ditemukan
+console.log(ganjilGenap()); //invalid data
 // *2 with build in function (split())
 
 function ganjilGenap2(plat) {
@@ -25,6 +67,12 @@ function ganjilGenap2(plat) {
         }
     }
 }
+
+// console.log(ganjilGenap2("2341;3429;864;1309;1276")); //plat genap sebanyak 2 dan plat ganjil sebanyak 3
+// console.log(ganjilGenap2("2347;3429;1305")); //plat ganjil sebanyak 3 dan plat genap tidak ditemukan
+// console.log(ganjilGenap2("864;1308;1276;1432")); //plat genap sebanyak 4 dan plat ganjil tidak ditemukan
+// console.log(ganjilGenap2("")); //plat tidak ditemukan
+// console.log(ganjilGenap2()); //invalid data
 
 // *3
 function ganjilGenap3(plat) {
@@ -54,13 +102,8 @@ function ganjilGenap3(plat) {
     }
 }
 
-console.log(ganjilGenap2("2341;3429;864;1309;1276")); //plat genap sebanyak 2 dan plat ganjil sebanyak 3
-console.log(ganjilGenap2("2347;3429;1305")); //plat ganjil sebanyak 3 dan plat genap tidak ditemukan
-console.log(ganjilGenap2("864;1308;1276;1432")); //plat genap sebanyak 4 dan plat ganjil tidak ditemukan
-console.log(ganjilGenap2("")); //plat tidak ditemukan
-console.log(ganjilGenap2()); //invalid data
-console.log(ganjilGenap3("2341;3429;864;1309;1276")); //plat genap sebanyak 2 dan plat ganjil sebanyak 3
-console.log(ganjilGenap3("2347;3429;1305")); //plat ganjil sebanyak 3 dan plat genap tidak ditemukan
-console.log(ganjilGenap3("864;1308;1276;1432")); //plat genap sebanyak 4 dan plat ganjil tidak ditemukan
-console.log(ganjilGenap3("")); //plat tidak ditemukan
-console.log(ganjilGenap3());
+// console.log(ganjilGenap3("2341;3429;864;1309;1276")); //plat genap sebanyak 2 dan plat ganjil sebanyak 3
+// console.log(ganjilGenap3("2347;3429;1305")); //plat ganjil sebanyak 3 dan plat genap tidak ditemukan
+// console.log(ganjilGenap3("864;1308;1276;1432")); //plat genap sebanyak 4 dan plat ganjil tidak ditemukan
+// console.log(ganjilGenap3("")); //plat tidak ditemukan
+// console.log(ganjilGenap2()); //invalid data
