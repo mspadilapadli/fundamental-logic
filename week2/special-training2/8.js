f3b6a09376a02d83b4f8ca9739acfa540b4e459d
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-09 07:47:13 +0700

    special-training 8

---
+++ week2/special-training2/8.js
@@ -0,0 +1,55 @@
+/*
+Buatlah sebuah function highestLowest untuk mendapatkan angka terbesar dan terkecil dari sebuah array multidimensi
+*/
+// * 1 manual
+
+// function highestLowest(arr) {
+//     // tulis kode disini
+//     let result = [];
+//     let min = Infinity;
+//     let max = 0;
+//     arr.forEach((indexI) => {
+//         indexI.forEach((indexJ) => {
+//             indexJ.forEach((indexK) => {
+//                 indexK < min ? (min = indexK) : min;
+//                 indexK > max ? (max = indexK) : max;
+//             });
+//         });
+//     });
+//     result.push(min, max);
+//     return result;
+// }
+
+//*2 flat()
+function highestLowest(arr) {
+    // tulis kode disini
+    let result = [];
+    let min = Infinity;
+    let max = 0;
+    arr.flat(2).forEach((el) => {
+        el < min ? (min = el) : min;
+        el > max ? (max = el) : max;
+    });
+    result.push(min, max);
+    return result;
+}
+
+let data = [
+    [
+        [4, 5, 6],
+        [9, 1, 2, 10],
+        [9, 4, 3],
+    ],
+    [
+        [4, 14, 31],
+        [9, 10, 18, 12, 20],
+        [1, 4, 90],
+    ],
+    [
+        [2, 5, 10],
+        [3, 4, 5],
+        [2, 4, 5, 10],
+    ],
+];
+
+console.log(highestLowest(data)); // [1, 90] => 1 yang terkecil dan 90 yang terbesar
