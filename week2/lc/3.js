564a17f55c0668011df1dc16c7aeffa4dfd5b960
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-10 08:19:30 +0700

    lc-3 - relese 1: done

---
+++ week2/lc/3.js
@@ -0,0 +1,36 @@
+function getTotalPrice(customer) {
+    const listProduct = {
+        "Asus ROG": 25000000,
+        "Lenovo Legion": 22000000,
+        "Zyrex 1945": 7000000,
+        "HP Omen": 20000000,
+        "Acer Predator": 21000000,
+    };
+    // Insert your code here
+
+    let prices = 0;
+    customer.products.forEach((item) => {
+        let [product, qty] = item;
+        prices += listProduct[product] * qty;
+    });
+
+    return customer.member ? prices * 0.1 : prices;
+}
+
+let customer = {
+    name: "Fajrin",
+    products: [
+        ["Asus ROG", 2],
+        ["Lenovo Legion", 3],
+    ],
+    member: true,
+};
+
+console.log(getTotalPrice(customer)); // 104400000
+/**
+ Proses
+ Fajrin membeli 2 Asus ROG, dimana harga satuan dari Asus ROG adalah 25000000 untuk membeli 2 Asus ROG dibutuhkan 50000000
+ Fajrin membeli 3 Lenovo Legion, dimana harga satuan dari Lenovo Legion adalah 22000000 untuk membeli 3Lenovo Legion dibutuhkan 66000000
+ Dari dua barang yang ingin dibeli Fajrin maka total belanja Fajrin adalah 116000000, dan dikarenakan Fajrin adalah member dari Tokosidia
+ maka akan diberikan 10% discount, oleh karena itu total yang harus dibayar oleh Fajrin adalah 104400000
+*/
