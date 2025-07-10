82e3ed729aa2e46d0b2d360cd5431cf6d001309f
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-10 08:57:47 +0700

    lc3-release 3: done

--- week2/lc/3.js
+++ week2/lc/3.js
@@ -1,3 +1,4 @@
+//* release 1
 function getTotalPrice(customer) {
     const listProduct = {
         "Asus ROG": 25000000,
@@ -17,16 +18,16 @@ function getTotalPrice(customer) {
     return customer.member ? prices * 0.1 : prices;
 }
 
-let customer = {
-    name: "Fajrin",
-    products: [
-        ["Asus ROG", 2],
-        ["Lenovo Legion", 3],
-    ],
-    member: true,
-};
+// let customer = {
+//     name: "Fajrin",
+//     products: [
+//         ["Asus ROG", 2],
+//         ["Lenovo Legion", 3],
+//     ],
+//     member: true,
+// };
 
-console.log(getTotalPrice(customer)); // 11600000
+// console.log(getTotalPrice(customer)); // 11600000
 /**
  Proses
  Fajrin membeli 2 Asus ROG, dimana harga satuan dari Asus ROG adalah 25000000 untuk membeli 2 Asus ROG dibutuhkan 50000000
@@ -35,7 +36,7 @@ console.log(getTotalPrice(customer)); // 11600000
  maka akan diberikan 10% discount, oleh karena itu total yang harus dibayar oleh Fajrin adalah 104400000
 */
 // ===== //
-
+//* release 2
 function getStock(items) {
     // Insert your code here
     let stock = {};
@@ -49,24 +50,85 @@ function getStock(items) {
     return stock;
 }
 
-console.log(
-    getStock([
-        "Asus ROG",
-        "Asus ROG",
-        "Asus ROG",
-        "Asus ROG",
-        "Lenovo Legion",
-        "Lenovo Legion",
-        "Lenovo Legion",
-        "HP Omen",
-        "HP Omen",
-    ])
-);
+// console.log(
+//     getStock([
+//         "Asus ROG",
+//         "Asus ROG",
+//         "Asus ROG",
+//         "Asus ROG",
+//         "Lenovo Legion",
+//         "Lenovo Legion",
+//         "Lenovo Legion",
+//         "HP Omen",
+//         "HP Omen",
+//     ])
+// );
 
 /**
  * {
  *  'Asus ROG': 4,
  *  'Lenovo Legion': 3,
  *  'HP Omen': 2
+ * }
+ */
+
+// ===== //
+
+//* release 3
+const customer = {
+    name: "Fajrin",
+    products: [
+        ["Asus ROG", 2],
+        ["Lenovo Legion", 3],
+    ],
+    member: true,
+};
+
+const items = [
+    "Asus ROG",
+    "Asus ROG",
+    "Asus ROG",
+    "Asus ROG",
+    "Lenovo Legion",
+    "Lenovo Legion",
+    "Lenovo Legion",
+    "HP Omen",
+    "HP Omen",
+];
+
+function shoppingTeros(customer, items) {
+    // Insert your code here
+    if (!customer) return "Tidak ada customer yang ingin melakukan transaksi";
+    let readyStock = getStock(items);
+    let isReadyStock;
+    customer.products.forEach((item) => {
+        let [product, qty] = item;
+
+        isReadyStock = readyStock[product] >= qty ? true : false;
+    });
+
+    if (!isReadyStock) return "Terdapat barang yang tidak tersedia di gudang";
+    let totalPrice = getTotalPrice(customer);
+    let message = customer.member
+        ? `Hai pelanggan setia ${customer.name}! Total harga yang harus kamu bayar adalah Rp ${totalPrice}`
+        : `Hai ${customer.name}! Total harga yang harus kamu bayar adalah Rp${totalPrice}`;
+    return {
+        name: customer.name,
+        products: customer.products,
+        member: customer.member,
+        message: message,
+    };
+}
+
+console.log(shoppingTeros(customer, items));
+/**
+ * {
+ *  name: 'Fajrin',
+ *  products: [
+ *   ['Asus ROG', 2],
+ *   ['Lenovo Legion', 3]
+ *  ],
+ *  member: true,
+ *  message: 'Hai pelanggan setia Fajrin! Total Harga yang harus kamu bayar adalah Rp 104400000
  * }
  */
