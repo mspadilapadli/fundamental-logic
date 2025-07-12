d0a4e201238cba7ddf2622501b3235c8b99b3c84
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-12 16:42:31 +0700

    serial_validator -  reversedOddIndex

--- week3/ch/1.serial_validator-ref.js
+++ week3/ch/1.serial_validator-ref.js
@@ -24,3 +24,57 @@ function splitString(str) {
 
 // console.log(splitString("124 765"));
 // // [ '124', '765' ]
+
+//=====//
+function reverseOddIndex(arr) {
+    // Your code here
+    let result = [];
+
+    // * manual forEadh - for
+    arr.forEach((num, index) => {
+        if (index % 2 != 0) {
+            // num = "a";
+
+            let reversedNum = "";
+            for (let i = 0; i < num.length; i++) {
+                reversedNum += num[num.length - 1 - i];
+            }
+            // console.log(num);
+            num = reversedNum;
+        }
+
+        result.push(num);
+    });
+
+    // * reversed()
+    // arr.forEach((num, index) => {
+    //     if (index % 2 != 0) {
+    //         // num = "a";
+    //         num = num.split("").reverse().join("");
+    //     }
+
+    //     result.push(num);
+    // });
+
+    // * manual nested forEach
+    // arr.forEach((num, index) => {
+    //     if (index % 2 != 0) {
+    //         let reversedNum = "";
+
+    //         [...num].forEach((char, index) => {
+    //             reversedNum += num[num.length - 1 - index];
+    //         });
+
+    //         num = reversedNum
+    //     }
+    //     result.push(num)
+    // });
+
+    return result;
+}
+
+console.log(reverseOddIndex(["79", "08", "86", "79"]));
+// [ '79', '80', '86', '97' ]
+
+// console.log(reverseOddIndex(["124", "765"]));
+// [ '124', '567' ]
