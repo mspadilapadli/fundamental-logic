027effb83177a60c4519f30ce9f3ff1bd9638ce7
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-16 08:30:21 +0700

    cari modus

---
+++ week3/uc/06.cariModus-ref.js
@@ -0,0 +1,29 @@
+function cariModus(arr) {
+    let modus = null;
+    let maxFreq = 0;
+
+    // *nested loop for
+    for (let i = 0; i < arr.length; i++) {
+        const indexI = arr[i];
+        let freq = 0;
+        for (let j = 0; j < arr.length; j++) {
+            const indexJ = arr[j];
+            if (indexI == indexJ) {
+                freq++;
+            }
+        }
+        if (freq > maxFreq) {
+            maxFreq = freq;
+            modus = arr[i];
+        }
+    }
+    if (maxFreq == 1 || maxFreq == arr.length) return -1;
+
+    return modus;
+}
+
+console.log(cariModus([10, 4, 5, 2, 4])); // 4
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
