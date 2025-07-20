b734ff8ae0a30a339303831a6f3bfe4a37fe3efb
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-20 07:55:48 +0700

    lc 2 : release 1 findMovies()

---
+++ week4/lc/2.js
@@ -0,0 +1,163 @@
+function findMovies(favoriteGenre) {
+    const movies = [
+        {
+            id: 1,
+            name: "Avengers end game",
+            genre: "Action",
+            soldTicket: 149,
+            capacity: 150,
+        },
+        {
+            id: 2,
+            name: "La la Land",
+            genre: "Romance",
+            soldTicket: 20,
+            capacity: 75,
+        },
+        {
+            id: 3,
+            name: "Beauty and the Beast",
+            genre: "Romance",
+            soldTicket: 50,
+            capacity: 50,
+        },
+        {
+            id: 4,
+            name: "Superman vs Batman",
+            genre: "Action",
+            soldTicket: 150,
+            capacity: 250,
+        },
+        {
+            id: 5,
+            name: "Transformer",
+            genre: "Action",
+            soldTicket: 90,
+            capacity: 90,
+        },
+        {
+            id: 6,
+            name: "5 feet apart",
+            genre: "Romance",
+            soldTicket: 25,
+            capacity: 45,
+        },
+        {
+            id: 7,
+            name: "Hamilton",
+            genre: "Musical",
+            soldTicket: 295,
+            capacity: 300,
+        },
+        {
+            id: 8,
+            name: "Dear Evan Hansen",
+            genre: "Musical",
+            soldTicket: 150,
+            capacity: 200,
+        },
+        {
+            id: 9,
+            name: "Conjuring",
+            genre: "Horror",
+            soldTicket: 30,
+            capacity: 100,
+        },
+        {
+            id: 10,
+            name: "Annabelle",
+            genre: "Horror",
+            soldTicket: 10,
+            capacity: 30,
+        },
+        {
+            id: 11,
+            name: "Fast and Furios",
+            genre: "Action",
+            soldTicket: 25,
+            capacity: 40,
+        },
+        {
+            id: 12,
+            name: "Romeo and Julet",
+            genre: "Romance",
+            soldTicket: 15,
+            capacity: 15,
+        },
+        {
+            id: 13,
+            name: "Wicked",
+            genre: "Musical",
+            soldTicket: 75,
+            capacity: 75,
+        },
+    ];
+    // Insert your code here
+    //* manual forEach()
+    let foundMovies = [];
+
+    favoriteGenre.forEach((genre) => {
+        movies.forEach((movie) => {
+            if (movie.genre == genre) foundMovies.push(movie);
+        });
+    });
+
+    return foundMovies;
+
+    //* filter() - includes()
+    //  return movies.filter((movie) => favoriteGenre.includes(movie.genre));
+}
+console.log(findMovies(["Action", "Musical"]));
+/**
+ [
+  {
+    id: 1,
+    name: 'Avengers end game',
+    genre: 'Action',
+    soldTicket: 149,
+    capacity: 150
+  },
+  {
+    id: 4,
+    name: 'Superman vs Batman',
+    genre: 'Action',
+    soldTicket: 150,
+    capacity: 250
+  },
+  {
+    id: 5,
+    name: 'Transformer',
+    genre: 'Action',
+    soldTicket: 90,
+    capacity: 90
+  },
+  {
+    id: 11,
+    name: 'Fast and Furios',
+    genre: 'Action',
+    soldTicket: 25,
+    capacity: 40
+  },
+  {
+    id: 7,
+    name: 'Hamilton',
+    genre: 'Musical',
+    soldTicket: 295,
+    capacity: 300
+  },
+  {
+    id: 8,
+    name: 'Dear Evan Hansen',
+    genre: 'Musical',
+    soldTicket: 150,
+    capacity: 200
+  },
+  {
+    id: 13,
+    name: 'Wicked',
+    genre: 'Musical',
+    soldTicket: 75,
+    capacity: 75
+  }
+]
+*/
---
+++ week4/lc/README.md
@@ -0,0 +1,45 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/0nQEMpd-)
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=13979251&assignment_repo_type=AssignmentRepo)
+# Live-Code-3-Phase-0
+
+## RULES
+
+1. Kerjakan secara individu. Segala bentuk kecurangan (mencontek ataupun diskusi) akan menyebabkan skor live code ini 0.
+1. Clone repo ini kemudian buatlah **branch dengan nama kalian**.
+1. Kerjakan pada file JavaScript (\*.js) dan HTML (\*.html) yang telah disediakan dan jangan melakukan perubahan terhadap file lain.
+1. Waktu pengerjaan:
+   - **120 menit** untuk **3 soal**.
+1. **Pada text editor hanya ada file yang terdapat pada repository ini**.
+1. Membuka referensi eksternal seperti Google, StackOverflow, dan MDN diperbolehkan.
+1. Dilarang membuka repository di organisasi tugas, baik pada organisasi batch sendiri ataupun batch lain, baik branch sendiri maupun branch orang lain (**setelah melakukan clone, close tab GitHub pada web browser kalian**).
+1. Lakukan `git push origin <branch-name>` dan create pull request **hanya jika waktu Live Code telah usai (bukan ketika kalian sudah selesai mengerjakan)**. Tuliskan nama lengkap kalian saat membuat pull request dan assign buddy.
+1. **Penilaian berbasis logika dan hasil akhir**. Pastikan keduanya sudah benar.
+1. **Khusus untuk nomer 1**, Pastikan output di browser kalian sudah sesuai dengan screenshot yang diberikan.
+
+## Notes
+
+- Live code ini memiliki testing. Pastikan telah mengeksekusi command `npm install` sebelum melakukan test. Command line untuk test adalah `npm test -- <nama_file>`.
+
+  | Soal | Command            | Max Score |
+  | ---- | ------------------ | --------- |
+  | 3    | Test di browser    | 5        |
+  | 1    | `npm test -- 2.js` | 25        |
+  | 2    | `npm test -- 3.js` | 70        |
+
+  Untuk soal nomor 3, karna soal berbentuk `html` dan `css`, kalian tidak perlu melakukan `npm test`, kalian dapat membandingkan dengan requirements yang di inginkan di gambar `1reference.png`
+
+- Built-in functions yang tidak dilarang oleh RULES:
+  - Basic primitive data types: `Number()`, `String()`, `Boolean()`
+  - Informatives: `typeof`, `.length`, `isInteger()`, `isArray()`, `isNaN()`
+  - Maths: `Math.floor()`, `Math.round()`, `Math.ceil()`, `Math.abs()`, `Math.sqrt()`, `Math.random()`
+  - Strings: `toString()`, `toUpperCase()`, `toLowerCase()`, `parseInt()`
+  - Object: `Object.keys()`, `Object.values()`
+  - Array: `push()`
+  <!-- - Jangan lupa untuk menyertakan pseudocode apabila diminta oleh soal. Apabila **pseudocode tidak disertakan**, mengakibatkan **nilai 0** untuk soal tersebut. -->
+- Reminder:
+  - **Live Code 1 = 10%**,
+  - **Live Code 2 = 20%**,
+  - **Live Code 3 = 25%**,
+  - **Daily challenges = 45%**,
+  <!-- - **Rocket 5%**. -->
+- **Passing score phase 0 = 61%**.
