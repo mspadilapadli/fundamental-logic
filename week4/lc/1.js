cb02d094780800d07661513cda7bb02a16c54d6f
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-20 07:30:02 +0700

    lc 1 : release 1 highestRating()

---
+++ week4/lc/1.js
@@ -0,0 +1,87 @@
+function highestRating(data) {
+    // Your code here
+    let highestRating;
+    let highestScore = 0;
+    data.forEach((movie) => {
+        let rating = movie.rating;
+        if (rating > highestScore) {
+            highestScore = rating;
+            highestRating = movie;
+        }
+    });
+
+    return highestRating;
+}
+
+let allMovies = [
+    {
+        title: "The Wages of Fear",
+        genre: "thriller",
+        director: "Henri-Georges Clouzot",
+        rating: 8.4,
+    },
+    {
+        title: "No Country for Old Men",
+        genre: "thriller",
+        director: "Coen Brothers",
+        rating: 8.1,
+    },
+    {
+        title: "500 Days of Summer",
+        genre: "romance",
+        director: "Marc Webb",
+        rating: 7.8,
+    },
+    {
+        title: "The Raid",
+        genre: "action",
+        director: "Gareth Evans",
+        rating: 7.6,
+    },
+    {
+        title: "The Raid 2",
+        genre: "action",
+        director: "Gareth Evans",
+        rating: 8.2,
+    },
+    {
+        title: "Senyap",
+        genre: "documentary",
+        director: "Joshua Oppenheimer",
+        rating: 8.3,
+    },
+    {
+        title: "Shoplifters",
+        genre: "drama",
+        director: "Kore-eda Hirokazu",
+        rating: 8.1,
+    },
+    {
+        title: "Hereditary",
+        genre: "horror",
+        director: "Ari Aster",
+        rating: 7.3,
+    },
+    {
+        title: "The Farewell",
+        genre: "drama",
+        director: "Lulu Wang",
+        rating: 7.7,
+    },
+    {
+        title: "Parasite",
+        genre: "thriller",
+        director: "Bong Joon-ho",
+        rating: 8.6,
+    },
+];
+
+console.log(highestRating(allMovies));
+/*
+{
+  title: 'Parasite',
+  genre: 'thriller',
+  director: 'Bong Joon-ho',      
+  rating: 8.6
+}
+*/
