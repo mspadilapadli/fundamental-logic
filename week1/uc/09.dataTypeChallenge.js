49aa8d78e25ff297b708ea4b50c263df8733412a
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-11-27 10:58:36 +0700

    fix the order of logic

--- week1/uc/09.dataTypeChallenge.js
+++ week1/uc/09.dataTypeChallenge.js
@@ -1,32 +1,33 @@
 // *1
+//good
 let kata = 0;
-if (!kata) {
+if (typeof kata === "boolean") {
+    kata
+        ? console.log(`thank you for agreeing`)
+        : console.log("cannot proceed without agreement");
+} else if (!kata) {
     console.log(`invalid data`);
 } else if (typeof kata == "string") {
     console.log(`username ${kata}`);
 } else if (typeof kata === "number") {
     console.log(`age ${kata}`);
-} else if (typeof kata === "boolean") {
-    kata
-        ? console.log(`thank you for agreeing`)
-        : console.log("cannot proceed without agreement");
 }
 
 // *2
-/*
-let kata = "";
-if (typeof kata == "boolean") {
-    kata
-        ? console.log(`thank you for greeting`)
-        : console.log(`cannot proceed without agreement`);
-} else if (typeof kata == "string") {
-    console.log(`username ${kata}`);
-} else if (typeof kata == "number" && kata !== 0 && !isNaN(NaN)) {
-    console.log(`age ${kata}`);
-} else {
-    console.log("Invalid data");
-}
-*/
+//minus empty string masih lolos
+// let kata = "";
+// if (typeof kata == "boolean") {
+//     kata
+//         ? console.log(`thank you for greeting`)
+//         : console.log(`cannot proceed without agreement`);
+// } else if (typeof kata == "string") {
+//     console.log(`username ${kata}`);
+// } else if (typeof kata == "number" && kata !== 0 && !isNaN(NaN)) {
+//     console.log(`age ${kata}`);
+// } else {
+//     console.log("Invalid data");
+// }
+
 // // create palindrome function
 // function palindrome(str) {
 //     let reversedStr = str.split("").reverse().join("");
@@ -36,7 +37,8 @@ if (typeof kata == "boolean") {
 // console.log(palindrome("kasurr rusak"));
 
 //*3
-// let kata = 0;
+//minus false masuk ke invalid data
+// let kata = false;
 // if (!kata) {
 //     console.log(`invalid data`);
 // } else if (typeof kata == "string") {
