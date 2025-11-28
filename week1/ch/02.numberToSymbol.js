abe8e21b0bab0e530e09204f37b6e796a15fc13b
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-11-28 20:56:44 +0700

    add logic dry code

--- week1/ch/02.numberToSymbol.js
+++ week1/ch/02.numberToSymbol.js
@@ -1,4 +1,4 @@
-let numbers = `1342`;
+let numbers = `1234`;
 if (!numbers || isNaN(numbers)) {
     console.log(`Input bukanlah angka`);
 } else {
@@ -15,9 +15,31 @@ if (!numbers || isNaN(numbers)) {
             }
         }
         let spesialCondition = indexI + +numbers[i - 1];
+        // console.log(spesialCondition);
         if (spesialCondition >= 5) {
             symbol += "$";
         }
+        console.log(symbol);
+    }
+}
+
+// * v2
+if (!numbers || isNaN(numbers)) {
+    console.log(`Input bukanlah angka`);
+} else {
+    for (let i = 0; i < numbers.length; i++) {
+        const indexI = +numbers[i];
+        let sym = indexI % 2 == 0 ? "@" : "#";
+        let symbol = "";
+
+        for (let j = 0; j < indexI; j++) {
+            symbol += sym;
+        }
+
+        if (indexI > 0 && indexI + +numbers[i - 1] >= 5) {
+            symbol += "$";
+        }
+
         console.log(symbol);
     }
 }
