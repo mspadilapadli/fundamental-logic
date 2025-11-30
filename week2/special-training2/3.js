ca02ae1714c0ff0eeba64a9e25641b58bbf9051b
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-11-30 09:37:27 +0700

    add one liner logic

--- week2/special-training2/3.js
+++ week2/special-training2/3.js
@@ -21,18 +21,24 @@ output:
 'hello'
 */
 
-function sumArr(arr) {
-    let result = "";
-    arr.forEach((el) => {
-        if (typeof el == "number") {
-            result = +el;
-        }
-        if (typeof el == "string") {
-            result += el;
-        }
-    });
+// function sumArr(arr) {
+//     let result = "";
+//     arr.forEach((el) => {
+//         if (typeof el == "number") {
+//             result = +el;
+//         }
+//         if (typeof el == "string") {
+//             result += el;
+//         }
+//     });
 
-    return result;
+//     return result;
+// }
+
+function sumArr(arr) {
+    //initial value tidak di tentukanm jadi otomatis initial value dari index 0
+    // reduce bisa dilakukan selama masih dalam 1 type data array yang sama, jika berbeda type data dalam 1 attay, maka pendekatannya akan berberda
+    return arr.reduce((r, e) => r + e);
 }
 
 console.log(sumArr([1, 2, 3, 4, 5])); // 15
