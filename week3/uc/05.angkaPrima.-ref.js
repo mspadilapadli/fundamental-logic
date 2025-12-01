5b2b659160a4a17f2ae5bfa325be26b81ae9e55d
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-12-01 19:25:26 +0700

    fix comparison operator

--- week3/uc/05.angkaPrima.-ref.js
+++ week3/uc/05.angkaPrima.-ref.js
@@ -1,5 +1,5 @@
 function angkaPrima(angka) {
-    if (angka <= 1) return false;
+    if (angka < 1) return false;
     for (let i = 2; i < angka; i++) {
         if (angka % i == 0) return false;
     }
