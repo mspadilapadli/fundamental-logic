e795d7db9b292abc616e9596dcb685bfe07098ce
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-12-05 09:40:24 +0700

    R1: findStrongest() : implisit & eksplisit reduce()

--- week4/slc/1.gottaGroup-ref.js
+++ week4/slc/1.gottaGroup-ref.js
@@ -1,50 +1,82 @@
-function findStrongest(arr) {
-    // Your code here
-    let strongestPokemon = "";
-    let strongestStatus = 0;
-    arr.forEach((pokemon) => {
-        let statusPokemon;
-        let status = pokemon.status;
-        let { attack, hp, defense } = status;
-        statusPokemon = attack + hp + defense;
-        if (statusPokemon > strongestStatus) {
-            strongestStatus = statusPokemon;
-            strongestPokemon = pokemon.name;
-        }
-    });
+//* release 1
+// function findStrongest(arr) {
+//     // Your code here
+//     let strongestPokemon = "";
+//     let strongestStatus = 0;
+//     arr.forEach((pokemon) => {
+//         let statusPokemon;
+//         let status = pokemon.status;
+//         let { attack, hp, defense } = status;
+//         statusPokemon = attack + hp + defense;
+//         if (statusPokemon > strongestStatus) {
+//             strongestStatus = statusPokemon;
+//             strongestPokemon = pokemon.name;
+//         }
+//     });
 
-    return strongestPokemon;
-}
+//     return strongestPokemon;
+// }
+//*2 eksplisit reduce ()
+// const findStrongest = (arr) => {
+//     const result = arr.reduce(
+//         (strongest, { name, status, type }) => {
+//             const { attack, hp, defense } = status;
+//             const totalStatus = attack + hp + defense;
+//             if (totalStatus > strongest.status) {
+//                 strongest.name = name;
+//                 strongest.status = totalStatus;
+//             }
+//             return strongest;
+//         },
+//         { name: "", status: 0 }
+//     );
+//     return result.name;
+// };
 
-// console.log(
-//     findStrongest([
-//         {
-//             name: "Charizard",
-//             status: { attack: 84, hp: 78, defense: 78 },
-//             type: "Flying",
-//         },
-//         {
-//             name: "Talonflame",
-//             status: { attack: 81, hp: 78, defense: 71 },
-//             type: "Flying",
-//         },
-//         {
-//             name: "Gengar",
-//             status: { attack: 65, hp: 60, defense: 60 },
-//             type: "Ghost",
-//         },
-//         {
-//             name: "Grimer",
-//             status: { attack: 80, hp: 80, defense: 50 },
-//             type: "Poison",
-//         },
-//         {
-//             name: "Arbok",
-//             status: { attack: 95, hp: 60, defense: 69 },
-//             type: "Poison",
-//         },
-//     ])
-// );
+//*3 implisit reduce->(arr.reduce(...).name)
+const findStrongest = (arr) =>
+    arr.reduce(
+        (strongest, { name, status, type }) => {
+            const { attack, hp, defense } = status;
+            const totalStatus = attack + hp + defense;
+            if (totalStatus > strongest.status) {
+                strongest.name = name;
+                strongest.status = totalStatus;
+            }
+            return strongest;
+        },
+        { name: "", status: 0 }
+    ).name;
+
+console.log(
+    findStrongest([
+        {
+            name: "Charizard",
+            status: { attack: 84, hp: 78, defense: 78 },
+            type: "Flying",
+        },
+        {
+            name: "Talonflame",
+            status: { attack: 81, hp: 78, defense: 71 },
+            type: "Flying",
+        },
+        {
+            name: "Gengar",
+            status: { attack: 65, hp: 60, defense: 60 },
+            type: "Ghost",
+        },
+        {
+            name: "Grimer",
+            status: { attack: 80, hp: 80, defense: 50 },
+            type: "Poison",
+        },
+        {
+            name: "Arbok",
+            status: { attack: 95, hp: 60, defense: 69 },
+            type: "Poison",
+        },
+    ])
+);
 // // Charizard
 //=================================//
 
@@ -106,25 +138,25 @@ console.log(
 }
 */
 
-console.log(
-    gottaGroupEmAll([
-        {
-            name: "Beedrill",
-            status: { attack: 72, hp: 73, defense: 75 },
-            type: "Flying",
-        },
-        {
-            name: "Gliscor",
-            status: { attack: 70, hp: 95, defense: 85 },
-            type: "Flying",
-        },
-        {
-            name: "Caterpie",
-            status: { attack: 30, hp: 45, defense: 55 },
-            type: "Bug",
-        },
-    ])
-);
+// console.log(
+//     gottaGroupEmAll([
+//         {
+//             name: "Beedrill",
+//             status: { attack: 72, hp: 73, defense: 75 },
+//             type: "Flying",
+//         },
+//         {
+//             name: "Gliscor",
+//             status: { attack: 70, hp: 95, defense: 85 },
+//             type: "Flying",
+//         },
+//         {
+//             name: "Caterpie",
+//             status: { attack: 30, hp: 45, defense: 55 },
+//             type: "Bug",
+//         },
+//     ])
+// );
 
 /*
 {
